95af7db5eee1c5fec34c0868774fa586f7b2300b
Thanuj <thanujdevil@gmail.com>
2024-11-28 21:03:26 +0530

    UI components added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import PlacesCard from "../components/ui/PlacesCard";
 import MapComponent from "../components/MapComponent";
 import FeaturesGrid from "../components/FeaturesGrid";
 import ProcessGrid from "../components/ProcessGrid";
+import Button from "../components/ui/button";
+import { MoveRight } from "lucide-react";
 
 const Home = () => {
   return (
@@ -25,7 +27,7 @@ const Home = () => {
         </h2>
         <div>
           <Swiper
-          className="pb-5"
+            className="pb-5"
             spaceBetween={40}
             slidesPerView={4}
             onSlideChange={() => console.log("slide change")}
@@ -49,18 +51,31 @@ const Home = () => {
       </div>
 
       <div className="mt-10">
-        <h2 className="text-center text-4xl font-bold">Services We <span className="text-mainColor ">Provide</span></h2>
-        <FeaturesGrid/>
+        <h2 className="text-center text-4xl font-bold">
+          Services We <span className="text-mainColor ">Provide</span>
+        </h2>
+        <FeaturesGrid />
       </div>
 
       <div className="mt-10">
-        <h2 className="text-center text-4xl font-bold">Things You <span className="text-mainColor ">Need</span></h2>
-        <p className="text-center mt-2">We want to make sure your vacation is safe, well-planned, and within your budget.</p>
-        <ProcessGrid/>
+        <h2 className="text-center text-4xl font-bold">
+          Things You <span className="text-mainColor ">Need</span>
+        </h2>
+        <p className="text-center mt-2">
+          We want to make sure your vacation is safe, well-planned, and within
+          your budget.
+        </p>
+        <ProcessGrid />
       </div>
-      
+
       <section>
-        <img src="" alt="" />
+        <img className="m-auto w-[300px]" src="/logo-tp.png" alt="" />
+        <h2 className="text-center text-4xl font-bold max-w-xl m-auto">
+          Get explanation. Track down locations. Increase your intelligence.
+        </h2>
+        <div className="flex justify-center mt-5  ">
+          <Button title="Start Now" />
+        </div>
       </section>
     </div>
   );
