29e8ae1e17356fe0825491a815e52a0c15cc4edc
Thanuj <thanujdevil@gmail.com>
2024-11-28 11:20:29 +0530

    Map component and home ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,17 @@ import About from "./pages/About";
 import Product from "./pages/Product";
 import Dashboard from "./pages/Dashboard";
 import Models from "./pages/Models";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
         <Route path="chat" element={<Chat />} />
         <Route path="about" element={<About />} />
         <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="pricing" element={<Pricing />} />ෆ
       </Route>
       <Route path="/admin" element={<AdminLayout />}>
         <Route path="dashboard" element={<Dashboard />} />
---
+++ src/components/HomeSlider.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {Autoplay} from "swiper/modules"
+import image1 from "../assets/slider/10863668.jpg";
+import image2 from "../assets/slider/Galle-1024x768.jpg";
+import image3 from "../assets/slider/Lotus-Tower-1.jpg";
+import image4 from "../assets/slider/Sigiriya.jpg";
+
+
+const HomeSlider = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false, 
+      }}
+      modules={[Autoplay]} 
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <img className="w-full h-[600px] object-cover" src={image1} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="w-full h-[600px] object-cover" src={image2} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="w-full h-[600px] object-cover" src={image3} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="w-full h-[600px] object-cover" src={image4} alt="" />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default HomeSlider;
